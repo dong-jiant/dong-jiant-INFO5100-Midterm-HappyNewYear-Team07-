96a4cf3b290b8eb319a28cb93f47c50f0f8488f2
Greenhouse-owner <905798204@qq.com>
2026-03-01 21:49:23 +0800

    add faculty course management panel
    Add ManageCoursesJPanel.form and ManageCoursesJPanel.java
    Implement course display and management UI for faculty

--- src/UserInterface/WorkAreas/AdminRole/ManagePersonnelWorkResp/ManagePersonsJPanel.java
+++ src/UserInterface/WorkAreas/AdminRole/ManagePersonnelWorkResp/ManagePersonsJPanel.java
@@ -7,6 +7,7 @@ package UserInterface.WorkAreas.AdminRole.ManagePersonnelWorkResp;
 
 import Business.Business;
 import Business.Person.Person;
+
 import java.awt.CardLayout;
 import javax.swing.JOptionPane;
 
